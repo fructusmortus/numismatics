2a44763931dceeb1b49851db5e3bec628bb948d4
Dima <fructusmortus@gmail.com>
2021-04-05 09:03:44 +0600

    Added custom_user app and templates login and logout

---
+++ custom_user/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class CustomUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=200, null=True)
+    address = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
