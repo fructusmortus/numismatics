1b94908cb0568064a09fb6034715f15145929613
Dima <fructusmortus@gmail.com>
2021-04-11 17:52:40 +0600

    Modified redirect url after registration in views.py

--- custom_user/views.py
+++ custom_user/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
-from home import views
+
 
 def register(response):
     if response.method == "POST":
         form = RegisterForm(response.POST)
         if form.is_valid():
             form.save()
-        return redirect(views.home)
+        return redirect("/")
     else:
         form = RegisterForm()
     return render(response, '../templates/registration/registration.html', {"form":form})
