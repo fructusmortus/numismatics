2d1b8e611e835da7e53b4bfaefe5f6df4612f0af
fructusmortus <45312075+fructusmortus@users.noreply.github.com>
2021-04-25 09:58:26 +0600

    Styling of add currency form (#16)
    * Styling of add currency form
    
    * pycodestyle fixes in forms.py

--- currency/forms.py
+++ currency/forms.py
@@ -1,9 +1,21 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Currency
 
 
-class CurrencyForm(ModelForm):
+class CurrencyForm(forms.ModelForm):
     class Meta:
         model = Currency
         fields = '__all__'
         exclude = ['user', 'thumbnail', 'date_created']
+
+        widgets = {
+                'item_type': forms.Select(attrs={'class': 'form-control'}),
+                'name': forms.TextInput(attrs={'class': 'form-control'}),
+                'code': forms.TextInput(attrs={'class': 'form-control'}),
+                'country': forms.TextInput(attrs={'class': 'form-control'}),
+                'release_date': forms.TextInput(attrs={'class': 'form-control'}),
+                'denomination': forms.TextInput(attrs={'class': 'form-control'}),
+                'quality': forms.Select(attrs={'class': 'form-control'}),
+                'series': forms.TextInput(attrs={'class': 'form-control'}),
+                'photo': forms.FileInput(attrs={'class': 'form-control'}),
+            }
