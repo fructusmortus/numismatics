996bac4ace6d4a84287295737ab89095d13f0250
Dima <fructusmortus@gmail.com>
2021-04-29 18:24:06 +0600

    Access permissions to update or delete

--- currency/views.py
+++ currency/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Currency
 from custom_user import models
 from .forms import CurrencyForm
@@ -9,6 +10,7 @@ def currency(request):
     return render(request, 'currency/all_currencies.html', {'currencies': currencies})
 
 
+@login_required
 def create_currency(request):
     form = CurrencyForm()
     if request.method == 'POST':
@@ -22,20 +24,20 @@ def create_currency(request):
     return render(request, 'currency/currency_form.html', context)
 
 
+@login_required
 def update_currency(request, pk):
     currency = Currency.objects.get(id=pk)
     form = CurrencyForm(instance=currency)
     if request.method == 'POST':
         form = CurrencyForm(request.POST, request.FILES, instance=currency)
         if form.is_valid():
-            currency = form.save(commit=False)
-            currency.user = request.user
             currency.save()
             return redirect('/currencies')
     context = {'form': form}
     return render(request, 'currency/currency_form.html', context)
 
 
+@login_required
 def delete_currency(request, pk):
     currency = Currency.objects.get(id=pk)
     if request.method == 'POST':
