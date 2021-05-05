13ff232a51a55f78d1958503f046c6ea66cd8362
Dima <fructusmortus@gmail.com>
2021-05-05 16:39:07 +0600

    Additional fields in user registration form and some styling

--- custom_user/forms.py
+++ custom_user/forms.py
@@ -1,12 +1,35 @@
 from django import forms
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import CustomUser
 
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
+
+    username = forms.CharField(
+        label="Username",
+        widget=forms.TextInput(attrs={'class':'form-control', 'type':'username'}),
+    )
+    email = forms.EmailField(
+        max_length=100, 
+        widget=forms.EmailInput(attrs={'class': 'form-control'}),
+        )
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={'class':'form-control', 'type':'password'}),
+    )
+    password2 = forms.CharField(
+        label="Confirm password",
+        widget=forms.PasswordInput(attrs={'class':'form-control', 'type':'password'}),
+    )
 
     class Meta:
-        model = User
-        fields = ["username", "email", "password1", "password2"]
+        model = CustomUser
+        fields = ["username", "first_name", "last_name", "address", "phone", "email", "password1", "password2"]
+
+        widgets = {
+                'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+                'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+                'address': forms.TextInput(attrs={'class': 'form-control'}),
+                'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            }
