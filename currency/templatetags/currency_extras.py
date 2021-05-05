ff085299d19d420e403c64eb1fe0cac235508ecf
Dima <fructusmortus@gmail.com>
2021-05-05 22:50:53 +0600

    PEP8 codestyle for currency_extras

--- currency/templatetags/currency_extras.py
+++ currency/templatetags/currency_extras.py
@@ -1,9 +1,10 @@
 from django import template
-from django.contrib.auth.models import Group 
+from django.contrib.auth.models import Group
 
-register = template.Library() 
+register = template.Library()
 
-@register.filter(name='is_group') 
+
+@register.filter(name='is_group')
 def is_group(user, group_name):
-    group =  Group.objects.get(name=group_name) 
+    group = Group.objects.get(name=group_name)
     return group in user.groups.all()
