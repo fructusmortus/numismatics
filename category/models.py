af8e24e89c7c399ca5f840dbb6d047dd9fe26331
Dima <fructusmortus@gmail.com>
2021-05-07 08:37:55 +0600

    Category model

--- category/models.py
+++ category/models.py
@@ -4,4 +4,16 @@ from django.db import models
 class Category(models.Model):
 
     name = models.CharField(max_length=256)
-    parent_id = models.IntegerField(null=True, blank=True)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+        # full_path = [self.name]                  
+        # k = self.parent_id
+        # while k is not None:
+        #     full_path.append(k.name)
+        #     k = k.parent_id
+        # return ' -> '.join(full_path[::-1])
