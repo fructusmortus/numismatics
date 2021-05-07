71ac12758a420a568cd6265526721434a103bd90
Dima <fructusmortus@gmail.com>
2021-05-07 08:37:55 +0600

    Category model

--- category/models.py
+++ category/models.py
@@ -4,4 +4,10 @@ from django.db import models
 class Category(models.Model):
 
     name = models.CharField(max_length=256)
-    parent_id = models.IntegerField(null=True, blank=True)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
