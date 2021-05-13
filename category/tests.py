6ffab6bc1b16d4ebdc79b7a1a18993e5de095a3d
Nargiza Sarkulova <nargiza.sarkulova@shopify.com>
2021-05-12 21:21:37 -0400

    Categories as nested dropdown options

--- category/tests.py
+++ category/tests.py
@@ -1,18 +1,42 @@
 from django.test import TestCase
+
+from tree.tree import Tree
 from .models import Category
+
 
 class CategoryTestCase(TestCase):
     def setUp(self):
-        Category.objects.create(name="Modern")
-        Category.objects.create(name="Medieval")
-        Category.objects.create(name="Austria", parent_id=1)
-        Category.objects.create(name="USSR", parent_id=1)
-        Category.objects.create(name="Greece", parent_id=2)
-        Category.objects.create(name="1920-1950 series", parent_id=4)
-        Category.objects.create(name="1950-1980 series", parent_id=4)
+        self.medieval = Category.objects.create(name="Medieval")
+        self.byzantine_empire = Category.objects.create(name="Byzantine Empire", parent=self.medieval)
+        self.roman_empire = Category.objects.create(name="Roman Empire", parent=self.medieval)
+        self.baden_gulden = Category.objects.create(name="Baden gulden", parent=self.roman_empire)
+        self.rhenish_guilder = Category.objects.create(name="Rhenish guilder", parent=self.roman_empire)
+
+        self.circulating = Category.objects.create(name="Circulating")
+        self.usd = Category.objects.create(name="United States dollar", parent=self.circulating)
+        self.british_pound = Category.objects.create(name="British pound", parent=self.circulating)
+        self.libyan_dinar = Category.objects.create(name="Libyan dinar", parent=self.circulating)
 
 
     def test_top_categories(self):
-        top_categories = Category.objects.filter(parent_id=None)
+        top_categories = Category.objects.filter(parent=None)
         category_names = [category.name for category in top_categories]
-        self.assertEqual(["Modern", "Medieval"], category_names)
+        self.assertEqual(["Medieval", "Circulating"], category_names)
+
+    def test_tree(self):
+        tree = Tree(Category)
+        expected = {
+            (self.medieval.id, "Medieval"): {
+                (self.byzantine_empire.id, "Byzantine Empire"): {},
+                (self.roman_empire.id, "Roman Empire"): {
+                    (self.baden_gulden.id, "Baden gulden"): {},
+                    (self.rhenish_guilder.id, "Rhenish guilder"): {}
+                }
+            },
+            (self.circulating.id, "Circulating"): {
+                (self.usd.id, "United States dollar"): {},
+                (self.british_pound.id, "British pound"): {},
+                (self.libyan_dinar.id, "Libyan dinar"): {}
+            }
+        }
+        self.assertEqual(expected, tree.get_tree())
--- currency/forms.py
+++ currency/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Currency
+from .utils import categories_to_dropdown_options
 
 
 class DateInput(forms.DateInput):
@@ -8,6 +9,7 @@ class DateInput(forms.DateInput):
 
 class CurrencyForm(forms.ModelForm):
     release_date = forms.DateField(widget=DateInput(attrs={'class': 'form-control'}))
+    category = forms.CharField(widget=forms.Select(choices=categories_to_dropdown_options()))
 
     class Meta:
         model = Currency
@@ -15,12 +17,12 @@ class CurrencyForm(forms.ModelForm):
         exclude = ['user', 'thumbnail', 'date_created']
 
         widgets = {
-                'item_type': forms.Select(attrs={'class': 'form-control'}),
-                'name': forms.TextInput(attrs={'class': 'form-control'}),
-                'code': forms.TextInput(attrs={'class': 'form-control'}),
-                'country': forms.Select(attrs={'class': 'form-control'}),
-                'denomination': forms.TextInput(attrs={'class': 'form-control'}),
-                'quality': forms.Select(attrs={'class': 'form-control'}),
-                'series': forms.TextInput(attrs={'class': 'form-control'}),
-                'photo': forms.FileInput(attrs={'class': 'form-control'}),
-            }
+            'item_type': forms.Select(attrs={'class': 'form-control'}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'code': forms.TextInput(attrs={'class': 'form-control'}),
+            'country': forms.Select(attrs={'class': 'form-control'}),
+            'denomination': forms.TextInput(attrs={'class': 'form-control'}),
+            'quality': forms.Select(attrs={'class': 'form-control'}),
+            'series': forms.TextInput(attrs={'class': 'form-control'}),
+            'photo': forms.FileInput(attrs={'class': 'form-control'}),
+        }
